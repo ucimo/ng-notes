67367958daf7de33bde58d5c740edb2f67b993a6
milos <obrovac.milos@gmail.com>
2020-03-05 13:46:19 +0100

    x

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  AfterViewInit
+} from "@angular/core";
+
+import { faWindowClose } from "@fortawesome/free-regular-svg-icons";
 
 @Component({
-  selector: 'app-note-card',
-  templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  selector: "app-note-card",
+  templateUrl: "./note-card.component.html",
+  styleUrls: ["./note-card.component.scss"]
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements OnInit, AfterViewInit {
+  faWindowClose = faWindowClose;
 
-  constructor() { }
+  @ViewChild("body") body: ElementRef<HTMLElement>;
+  @ViewChild("bodyText") bodyText: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    // let style = window.getComputedStyle(this.cardBody.nativeElement, null);
+    // // let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+    // // let scrollableHeight = parseInt(style.getPropertyValue("scrollHeight"), 10);
+    // console.log(style, "body  text");
+    // console.log(viewableHeight, scrollableHeight);
+  }
+  ngAfterViewInit() {
+    // let bodyStyle = window.getComputedStyle(this.body.nativeElement, null);
+    // let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+    // let scrollableHeight = parseInt(style.getPropertyValue("scrollHeight"), 10);
+
+    console.log(this.body.nativeElement.style.height, "body  h");
   }
 
+  consoleBody() {
+    console.log(this.body, "body  text");
+    console.log(this.body.nativeElement.scrollHeight, "body  scrollHeight");
+    console.log(this.body.nativeElement.offsetHeight, "body  offsetHeight");
+  }
 }
