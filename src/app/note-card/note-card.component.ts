87d06ae9356aba668ae5b4ed1c936706a19cb930
milos <frogs.n.grandmas@gmail.com>
2020-03-06 10:59:33 +0100

    most of the layout is finished

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -4,7 +4,8 @@ import {
   ViewChild,
   ElementRef,
   Renderer2,
-  AfterViewInit
+  AfterViewInit,
+  Input
 } from "@angular/core";
 
 import { faWindowClose } from "@fortawesome/free-regular-svg-icons";
@@ -17,35 +18,37 @@ import { faWindowClose } from "@fortawesome/free-regular-svg-icons";
 export class NoteCardComponent implements OnInit, AfterViewInit {
   faWindowClose = faWindowClose;
 
-  @ViewChild("body") body: ElementRef<HTMLElement>;
+  @Input() title: string;
+  @Input() body: string;
+
+  @ViewChild("bodyContainer") bodyContainer: ElementRef<HTMLElement>;
   @ViewChild("textFadeout") textFadeout: ElementRef<HTMLElement>;
 
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
-    // let style = window.getComputedStyle(this.body.nativeElement, null);
+    // let style = window.getComputedStyle(this.bodyContainer.nativeElement, null);
     // let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
     // let scrollableHeight = parseInt(style.getPropertyValue("scrollHeight"), 10);
-    // console.log(style, "body  text");
+    // console.log(style, "bodyContainer  text");
     // console.log(viewableHeight, scrollableHeight);
-    // console.log(
-    //   this.body.nativeElement.offsetHeight,
-    //   "on init body  offsetHeight",
-    //   this.body.nativeElement.scrollHeight,
-    //   "on init body  scrollHeight"
-    // );
+    // const { offsetHeight, scrollHeight } = this.bodyContainer.nativeElement;
+    // console.log(offsetHeight, scrollHeight);
+    // if (offsetHeight >= scrollHeight) {
+    //   // if there is no text overflow
+    //   this.renderer.setStyle(this.textFadeout.nativeElement, "display", "none");
+    // }
   }
   ngAfterViewInit() {
-    const { offsetHeight, scrollHeight } = this.body.nativeElement;
-    console.log(offsetHeight, scrollHeight);
-    if (offsetHeight > scrollHeight) {
+    const { offsetHeight, scrollHeight } = this.bodyContainer.nativeElement;
+    if (offsetHeight >= scrollHeight) {
       // if there is no text overflow
       this.renderer.setStyle(this.textFadeout.nativeElement, "display", "none");
     }
   }
 
   consoleBody() {
-    const { offsetHeight, scrollHeight } = this.body.nativeElement;
+    const { offsetHeight, scrollHeight } = this.bodyContainer.nativeElement;
     console.log(offsetHeight, scrollHeight);
   }
 }
